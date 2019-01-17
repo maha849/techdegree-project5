7fe23203b9e3220a4b02feabf826df31e1eb4c8d
maha849 <maharshi849@gmail.com>
2019-01-17 10:53:23 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 
 
 $('input').on('keyup', function(){
-  const input = $('input').val();
+  const input = $('input').val().toLowerCase();
   for(let i=0; i < $('a').length; i+=1){
     let $a = $('a').eq(i);
-    let content = $a.attr("data-title");
+    let content = $a.attr("data-title").toLowerCase();
     if(content.indexOf(input) == -1){
       $a.css("display", "none");
     }else{
